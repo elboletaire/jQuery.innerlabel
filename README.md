bbe834db870aefd8094226bdb339fb206ebeb7f6
Òscar Casajuana <elboletaire@gmail.com>
2011-12-24 19:22:15 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,54 @@
+#About
+**jQuery Inner Label** is a plug-in that lets you easily convert your labels into crossbrowser html5 placeholders (it uses html4 + css when html5 placeholder is not possible).
+
+## Syntax
+    jQuery(function($){
+		// Default options
+		$('label').innerlabel();
+		
+		// Disable html5 placeholders
+		$('label').innerlabel({
+			placeholder: false
+		});
+	});
+
+## Default options
+	jQuery(function($){
+		$('label').innerlabel({
+			label_class: "inner-label", // class applied to label
+			wrapper: false, // is the label yet wrapped? specify it's wrapper tag here
+			wrapper_class: "inner-label-wrapper", // class applied to wrapper
+			active_class: "active", // class added to wrapper when input is active
+			possible_wrappers: [
+				'div',
+				'li',
+				'dd'
+			],
+			possible_inputs: [
+				'input',
+				'textarea'
+			],
+			placeholder: true // enables placeholder if available
+		});
+	});
+
+## Notes
+Also remember to add the css to your site.
+
+You can style each inner-label using CSS:
+	#my-container .user-form .inner-label {
+		color: red;
+	}
+
+## Compatibility
+
+Works with jQuery versions 1.6 to 1.7.1. In previous versions works but buggy (maybe 'bind' or 'trigger' issues).
+
+Must work under every browser. 
+
+Tested under (Win, Linux & Mac)...
+ * IE 6 to 9
+ * FF 7 & 8
+ * Opera 11.6
+ * Safari (5.1 under Mac and 5.1.2 under Win)
+ * Chrome 16
---
+++ jquery.innerlabel.js
@@ -0,0 +1,138 @@
+/**
+ * A simple jQuery plugin to use labels as placeholders
+ * v. 0.1 2010-03-06
+ * v. 0.2 2010-05-15 - minnor bugfixes
+ * v. 0.3 2010-08-02 - minnor bugfixes
+ * v. 1.0 2011-12-24 - Fully redeveloped and added html5 placeholder support
+ * @author Oscar Casajuana a.k.a. elboletaire <elboletaire {at} underave {dot} net> 
+ * 
+ * Usage:
+ * 
+ * jQuery('#form label').innerlabel();
+ * 
+ * Options you can set:
+ * 
+ * jQuery('#form label').innerlabel({
+ * 		defaultClass:	"inner-label", // Default class to be applied to labels
+
+ * });
+ * 
+ */
+(function($){
+	$.fn.innerlabel = function(options){
+		
+		$.fn.innerlabel.defaults = {
+			label_class: "inner-label",
+			wrapper: false,
+			wrapper_class: "inner-label-wrapper",
+			active_class: "active",
+			possible_wrappers: [
+				'div',
+				'li',
+				'dd'
+			],
+			possible_inputs: [
+				'input',
+				'textarea'
+			],
+			placeholder: true // enables placeholder if available
+		}
+		
+		var opts = $.extend({}, $.fn.innerlabel.defaults, options);
+		
+		// Array to string conversion
+		opts.possible_wrappers = opts.possible_wrappers.join(',');
+		opts.possible_inputs = opts.possible_inputs.join(',');
+		
+		var innerlabel = function(label) {
+			var self = this, label = jQuery(label), input, wrapper, has_placeholder;
+
+			var innerize = {
+				// Converts label to input placeholder
+				placeholder: function() {
+					input.attr("placeholder", label.text());
+					label.remove();
+				},
+				// Wrapps input and label
+				wrap: function() {
+					// Check for a unique wrapper
+					wrapper = opts.wrapper;
+					if (wrapper || parseInt(label.parent(opts.possible_wrappers).children().length) === 2) {
+						wrapper = label.parent(wrapper || opts.possible_wrappers);
+					// Otherwise create it
+					} else {
+						wrapper = $('<div></div>');
+						var to_add = input;
+						if (!label.next(opts.possible_inputs).length && !label.prev(opts.possible_inputs).length) {
+							to_add = input.parent();
+						}
+						label.add(to_add).wrapAll(wrapper);
+					}
+					return wrapper.addClass(opts.wrapper_class).css( { position: "relative" , overflow: "hidden"  } );
+				},
+				// Get input field
+				getInput: function() {
+					// label has an input assigned
+					if (label.attr('for')) {
+						input = $('#' + label.attr('for'));
+					}
+					// label surrounds input
+					else {
+						input = label.find(opts.possible_inputs);
+					}
+					// Detect placeholder attribute
+					has_placeholder = !! (input[0].placeholder === "") && !! (input[0].placeholder !== undefined)
+				},
+				init: function() {
+					this.getInput();
+					if (has_placeholder && opts.placeholder) {
+						innerize.placeholder();
+						return;
+					}
+					
+					// Add default class to label
+					if (!has_placeholder || !opts.placeholder) {
+						label.addClass(opts.label_class);
+					}
+					
+					var wrapper = this.wrap();
+					
+					var logic = {
+						show: function() {
+							if (input.val() == '') {
+								label.show();
+								wrapper.removeClass(opts.active_class);
+							}
+						},
+						hide: function() {
+							label.hide();
+							wrapper.addClass(opts.active_class);
+						}
+					}
+					
+					// Add logic to label and input
+					input.bind('focus', logic.hide).bind('blur', logic.show);
+					label.bind('click', function(){input.focus();});
+					// Disable label text selection
+					label.bind('mousedown', function(e) {input.focus(); e.preventDefault();});
+
+					// This is for hidding the label when input is not empty (at load page)
+					if (input.val() != '') {
+						logic.hide();
+					} else {
+						input.trigger('blur');
+					}
+
+				}
+			}
+			innerize.init();
+		}
+		
+		return this.each(function(){
+			if (!$(this).data('inner-label')) {
+				$(this).data('inner-label', true);
+				new innerlabel(this);
+			}
+		});
+	}
+})(jQuery);
