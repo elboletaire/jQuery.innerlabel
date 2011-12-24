c681a845b53fa301468e4caea7c076117950c033
Òscar Casajuana <elboletaire@gmail.com>
2011-12-24 19:43:21 +0100

    Added license

--- jquery.innerlabel.js
+++ jquery.innerlabel.js
@@ -1,10 +1,24 @@
 /**
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ *
  * A simple jQuery plugin to use labels as placeholders
  * v. 0.1 2010-03-06
  * v. 0.2 2010-05-15 - minnor bugfixes
  * v. 0.3 2010-08-02 - minnor bugfixes
  * v. 1.0 2011-12-24 - Fully redeveloped and added html5 placeholder support
- * @author Oscar Casajuana a.k.a. elboletaire <elboletaire {at} underave {dot} net> 
+ * @author Oscar Casajuana a.k.a. elboletaire <elboletaire {at} underave {dot} net>  
  * 
  * Usage:
  * 
